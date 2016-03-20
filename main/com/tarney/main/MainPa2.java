1d00b9af184c012a92ad8a63a6053d6aef246c0a
1amBulletproof <brandon.tarney@gmail.com>
2016-03-20 13:49:25 -0400

    PA2 Project structure initial setup

---
+++ main/com/tarney/main/MainPa2.java
@@ -0,0 +1,12 @@
+package com.tarney.main;
+
+public class MainPa2 {
+	
+	//TODO update main program for final product
+	public static void main(String[] args) {
+		
+		System.out.println("test");
+		
+	}
+
+}
