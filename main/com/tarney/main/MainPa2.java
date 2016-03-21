69fa3f9833cdbbf5f278d2bdb627abcf34b5bd3c
1amBulletproof <brandon.tarney@gmail.com>
2016-03-21 16:30:55 -0400

    Added output message at each step

--- main/com/tarney/main/MainPa2.java
+++ main/com/tarney/main/MainPa2.java
@@ -21,7 +21,7 @@ public class MainPa2 {
 		dblStack.pushA("a1");
 		dblStack.pushB("b1");
 		
-		displayStacks();
+		displayStacks("pushed 1 item to stackA and 1 item to stackB");
 		
 		dblStack.pushA("a2");
 		dblStack.pushB("b2");
@@ -31,12 +31,12 @@ public class MainPa2 {
 		dblStack.multiPopA(1);
 		dblStack.multiPopB(1);
 		
-		displayStacks();
+		displayStacks("pushed 2 more items to each stack and then popped 1 item from each stack");
 		
 		dblStack.multiPopA(2);
 		dblStack.multiPopB(2);
 		
-		displayStacks();
+		displayStacks("popped 2 items from each stack (each stack had 2 items previously)");
 		
 		dblStack.pushA("a1");
 		dblStack.pushB("b1");
@@ -47,16 +47,17 @@ public class MainPa2 {
 		
 		dblStack.transfer(1);
 		
-		displayStacks();
+		displayStacks("pushed 3 items to each stack and then transfered 1 item from stackA to stackB");
 		
 		dblStack.transfer(2);
 		
-		displayStacks();
+		displayStacks("transfered 2 more items from stackA to stackB");
 		
 	}
 	
-	public static void displayStacks() {
+	public static void displayStacks(String whatDidYouDo) {
 		
+		System.out.println(whatDidYouDo);
 		System.out.println("StackA followed by StackB:");
 		dblStack.getStackA().printStack();
 		dblStack.getStackB().printStack();
